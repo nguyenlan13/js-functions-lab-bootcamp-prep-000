b88a8315d5a226cc0b3232781c1255e1806d6115
Lan Nguyen <nguyenlan13@gmail.com>
2019-01-28 05:19:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays (string){
+  return "Happy holidays!";
+}
+
+
+function happyHolidaysTo (name){
+  return string.you("Happy holidays, ${name}!");
+  
+}
+
+
+
+function happyHolidayTo (holiday, name){
+  return string("Happy ${holiday}, ${name}!");
+}
+
+function holidayCountdown (days, holiday){
+  return string("It\'s ${days} days until ${holiday}!" );
+}
